b1a6797a0af67349c7a30c5a7aaa418a04bbdec3
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-02-15 11:39:00 +0200

    First commit, message component and axios

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <message>Hello Vue</message>
+
+    <ul>
+      <li v-for="comic in comics">
+        <p>Resource: {{comic.resourceURI}}</p>
+        <p>Name: {{comic.name}}</p>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import Message from './components/Message';  // note the import of the component (you bring it in)
+import Axios from 'axios';
+export default {
+  name: 'app',
+  components: { Message },  // AND also note the listing of the component (you use it)
+  data () {
+    return {
+      comics: []
+    }
+  },
+  mounted() {
+    Axios.get('https://gateway.marvel.com:443/v1/public/characters/1011334?apikey=2613d3beb415606554f219d832a23901')
+      .then(response => this.comics = response.data.data.results[0].comics.items );
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box is-info">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+  .box { background: lightgray; padding: 10px; border: 1px solid black; margin-bottom: 1rem; }
+</style>
